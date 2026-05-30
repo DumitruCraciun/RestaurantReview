1b3cb9cd77aad4d38c4d5095b9d55cb3cd23c696
Dumitru Craciun <mediainfo_on@yahoo.com>
2026-05-30 13:04:27 +0100

    Fix route to API

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,1 +1,3 @@
-export const API_ENDPOINT = process.env.REACT_APP_API_URL || "http://localhost:3000";
+const API_ENDPOINT = process.env.REACT_APP_API_URL || "http://localhost:3000";
+
+export { API_ENDPOINT };
